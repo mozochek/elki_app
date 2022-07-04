4cd9efd1d4ca9ee97a4e3c53261537b0cebd4e1d
Kirill Fokin <mozochek@gmail.com>
2022-07-04 11:18:00 +0300

    Wrapped app with runZonedGuarded

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
-import 'package:elki_app/modules/app.dart';
+import 'dart:async';
+import 'dart:developer' as dev;
 
-Future<void> main() async {
-  await App.initializeAndRun();
+import 'package:elki_app/modules/app.dart';
+import 'package:flutter/foundation.dart';
+
+void main() {
+  runZonedGuarded(
+    () async {
+      await App.initializeAndRun();
+    },
+    (e, stackTrace) {
+      if (kDebugMode) {
+        dev.log(e.toString(), stackTrace: stackTrace);
+      }
+    },
+  );
 }
